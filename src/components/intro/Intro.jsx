3a6782a2d3e8c6e41f57cbe5142cfe7373418b04
Emily Garcia <garcia.16.emily@gmail.com>
2021-09-22 22:04:51 -0500

    Create introduccion page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/woman.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Emily Garcia</h1>
+                    <h3>Freelance <span></span></h3>
+                </div>
+                <a href="#portafolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
