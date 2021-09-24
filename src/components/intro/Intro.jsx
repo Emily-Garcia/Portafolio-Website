dffaaff6c5815dd727d41ff90b75ff45e0cfcfe3
Emily Garcia <garcia.16.emily@gmail.com>
2021-09-23 22:10:51 -0500

    Create text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect,useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer","UI Designer", "UX Designer"]
+        })
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Emily Garcia</h1>
-                    <h3>Freelance <span></span></h3>
+                    <h3>I'm a <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portafolio">
                     <img src="assets/down.png" alt="" />
